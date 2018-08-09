84fcb3f4cf4c6b67bff34eabd34badd0218eedee
SolalVall <solal.vallee@laposte.net>
2018-08-09 14:38:35 +0200

    update coin class and argument traitment

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ def index():
     error = None
     if request.method == 'POST':
         coinTrigramme = request.form['cryptoTrigramme']
-        apiUrl = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + coinTrigramme + '&tsyms=EUR'
-        selected_crypto = Coin(coinTrigramme, apiUrl)
+        coinDevise = request.form['cryptoDevise'] 
+        apiUrl = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + coinTrigramme + '&tsyms=' + coinDevise
+        selected_crypto = Coin(coinTrigramme, coinDevise, apiUrl)
 
         cryptoInfo = requests.get(apiUrl)
         cryptoInfoJson = cryptoInfo.json()
         #print cryptoInfoJson
-        selected_crypto.price = cryptoInfoJson['RAW'][coinTrigramme]['EUR']['PRICE']    
-        selected_crypto.openDay = cryptoInfoJson['RAW'][coinTrigramme]['EUR']['OPENDAY']
-        selected_crypto.highDay = cryptoInfoJson['RAW'][coinTrigramme]['EUR']['HIGHDAY'] 
-        selected_crypto.lowDay = cryptoInfoJson['RAW'][coinTrigramme]['EUR']['LOWDAY']
-        selected_crypto.marketCap = cryptoInfoJson['RAW'][coinTrigramme]['EUR']['MKTCAP'] 
-        selected_crypto.evolutionDay = cryptoInfoJson['RAW'][coinTrigramme]['EUR']['CHANGEPCTDAY']
+        selected_crypto.price = cryptoInfoJson['RAW'][coinTrigramme][coinDevise]['PRICE']    
+        selected_crypto.openDay = cryptoInfoJson['RAW'][coinTrigramme][coinDevise]['OPENDAY']
+        selected_crypto.highDay = cryptoInfoJson['RAW'][coinTrigramme][coinDevise]['HIGHDAY'] 
+        selected_crypto.lowDay = cryptoInfoJson['RAW'][coinTrigramme][coinDevise]['LOWDAY']
+        selected_crypto.marketCap = cryptoInfoJson['RAW'][coinTrigramme][coinDevise]['MKTCAP'] 
+        selected_crypto.evolutionDay = cryptoInfoJson['RAW'][coinTrigramme][coinDevise]['CHANGEPCTDAY']
 
         return render_template('index.html', selected_crypto=selected_crypto, error=error)
     else:
@@ -45,7 +46,7 @@ if __name__ == '__main__':
 #	time.sleep(5)
 #	timer += 5
 #	r = requests.get(url).json()
-#	array_eth_price.append(r['EUR'])
+#	array_eth_price.append(r[coinDevise])
 #	
 #	if(len(array_eth_price) > 1):
 #		variation_pourcentage = (float(array_eth_price[-1]-array_eth_price[0])/float(array_eth_price[0]))*100
--- coin.py
+++ coin.py
@@ -1,6 +1,14 @@
+# coding: utf-8
 class Coin:
-    def __init__(self, trigramme, apiUrl, price=None, openDay=None, highDay=None, lowDay=None, marketCap=None, evolutionDay=None):
+    def __init__(self, trigramme, devise, apiUrl, symbol=None,price=None, openDay=None, highDay=None, lowDay=None, marketCap=None, evolutionDay=None):
         self.trigramme = trigramme
+        self.devise = devise
+        
+        if(self.devise == 'USD'):
+            self.symbol = '$'
+        else:
+            self.symbol = self.devise
+        
         self.url = apiUrl
         self.price = 0
         self.openDay = 0
