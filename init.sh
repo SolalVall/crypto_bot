872fe87b7a55969c077b3f646446687716e1907c
SolalVall <solal.vallee@laposte.net>
2018-08-10 12:02:07 +0200

    permission update for init script (sudo)

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ then
 	echo "Pip is not installed\n"
 	echo -e "Download get-pip script:"
 	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-	python get-pip.py
+	sudo python get-pip.py --user
 else
 	echo -e "Pip is already installed: \n"
 	pip --version
@@ -18,11 +18,12 @@ fi
 if [ -z "$packagesExists" ]
 then
 	echo -e "\nPackages are not installed"
-	pip install Flask, requests
+	sudo pip install Flask, requests
 else
 	echo -e "\nPackages are already installed:\n"
 	echo -e "$packagesExists \n"
 fi
 
 # Run app.py with flask command
+echo -e "\n---------------------START APP---------------------\n"
 flask run
