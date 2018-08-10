627966ee38f3ed9ee42dd1ea41705c5f22932d27
SolalVall <solal.vallee@laposte.net>
2018-08-10 11:54:40 +0200

    update init script

--- init.sh
+++ init.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
+pipExist=$(which pip)
+packagesExists=$( pip list | grep 'Flask\|requests' )
 
 #Check if pip is installed
-pipExist=$(which pip)
 if [ -z "$pipExist" ]
 then
-	echo "Pip is not installed"
+	echo "Pip is not installed\n"
+	echo -e "Download get-pip script:"
+	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	python get-pip.py
 else
 	echo -e "Pip is already installed: \n"
 	pip --version
 fi
 
-packagesExists=$( pip list | grep 'Flask\|requests' )
+#Check if necessary packages are installed
 if [ -z "$packagesExists" ]
 then
 	echo -e "\nPackages are not installed"
+	pip install Flask, requests
 else
 	echo -e "\nPackages are already installed:\n"
 	echo -e "$packagesExists \n"
 fi
-# Check if request and flask already installed
-#pip list | grep -F requests flask
 
-# If not install
-#sudo pip install request flask
-
-# Run app.py qith flask command
-#flask run
+# Run app.py with flask command
+flask run
