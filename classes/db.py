992e24bbf7c38298940855c27f9a51074ef2a286
SolalVall <solal.vallee@laposte.net>
2018-09-18 22:34:31 +0200

    form verifications are ok for the moment

--- classes/db.py
+++ classes/db.py
@@ -1,3 +1,4 @@
+import re
 class Database:
     from pymongo import MongoClient
 
@@ -11,14 +12,29 @@ class Database:
 	
     def create_user(self, user):
         #Verify register form
-        #Iterate over attributes of User Object
+        #Iterate over attributes of User Object (check empty field)
         for attribute, value in user.__dict__.iteritems():
             print attribute, value
             if value == "" :
                 errorMessage = "Please insert a value for " + attribute
                 return errorMessage
+
+        #Check quality of field
+        regex_pseudo = re.compile(r'^[A-Za-z0-9_-]{5,20}$')
+        regex_email = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+        regex_password = re.compile(r'[A-Za-z0-9@#$%^&+=]{8,}')
+        if regex_pseudo.search(user.username) is None:
+            errorMessage = "Invalid Pseudo (5-20 characters long / - _ are authorized)"
+            return errorMessage
+        elif regex_email.search(user.email) is None:
+            errorMessage = "Please insert a valid mail adress"
+            return errorMessage
+        elif regex_password.search(user.password) is None:
+            errorMessage = "Password must have at least 8 characters"
+            return errorMessage
+            
+        #Add user to db
         else:
-            #Add user to db
             new_user = {"username": user.username,
                         "email": user.email,
                         "password": user.password}
