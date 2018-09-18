bd14567214f90c9fbfea11f77fde5ae5402a46b8
SolalVall <solal.vallee@laposte.net>
2018-09-18 17:00:55 +0200

    Add a user to mongo with pymongo works !

---
+++ classes/db.py
@@ -0,0 +1,16 @@
+class Database:
+    from pymongo import MongoClient
+
+    def __init__(self):
+        #Initialize connection
+        self.client = self.MongoClient('localhost', 27017)
+        #Create db
+        self.db = self.client.cryptobot_database
+        #Create collection
+        self.cryptobot_users = self.db.cryptobot_users
+
+    def create_user(self, user):
+        new_user = {"username": user.name,
+                    "email": user.email,
+                    "password": user.password}
+        self.new_user_id = self.cryptobot_users.insert_one(new_user).inserted_id 
